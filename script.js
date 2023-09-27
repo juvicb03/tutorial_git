9da4158865e4053ae7c8f55c6122af4b28de4250
juvicb03 <jjoaovcb@hotmail.com>
2023-09-27 11:08:00 -0300

    fix: novo hotel

--- script.js
+++ script.js
@@ -36,6 +36,13 @@ const hotelData = [
     pricing: 600.0,
     rate: 5,
   },
+  {
+    local: "Fortaleza",
+    name: "Hotel Villa Reali",
+    src: "https://images.pexels.com/photos/594077/pexels-photo-594077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    pricing: 250.0,
+    rate: 3.6,
+  },
 ];
 
 const hotelListComponent = document.getElementById("card-list");
